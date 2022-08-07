3f12e3a320ab18b809f5011f57474159d73945eb
@Draichi <lucasdraichi@gmail.com>
2022-08-07 02:33:51 -0300

    feat: creates particles

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,79 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import particleTextureImage from "./textures/particles/4.png";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const canvas =
+  (document.querySelector("canvas#webgl") as HTMLElement) ||
+  ({} as HTMLElement);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const scene = new THREE.Scene();
+
+const textureLoader = new THREE.TextureLoader();
+const particleTexture = textureLoader.load(particleTextureImage);
+
+const particlesMaterial = new THREE.PointsMaterial({
+  size: 0.1,
+  sizeAttenuation: true,
+  transparent: true,
+  alphaMap: particleTexture,
+  alphaTest: 0.001,
+  color: 0xcc00ff,
+});
+const particlesGeometry = new THREE.BufferGeometry();
+const particlesCount = 5000;
+const particlesWithCoordinatesArraySize = particlesCount * 3;
+const particlesPosition = new Float32Array(particlesWithCoordinatesArraySize);
+for (let i = 0; i < particlesWithCoordinatesArraySize; i++) {
+  particlesPosition[i] = (Math.random() - 0.5) * 10;
+}
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(particlesPosition, 3)
+);
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  1,
+  100
+);
+camera.position.set(0, 5, 4);
+scene.add(camera);
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+
+tick();
